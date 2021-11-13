4e6df0322cd9e8ecd5c95bfbbc8567770a15f105
Vixci <ciupecvictor@gmail.com>
2021-11-13 21:45:34 +0100

    Added OrderRepository to update the database

---
+++ PackagingCalculator/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PackagingCalculator.Entities;
+
+namespace PackagingCalculator.Repositories
+{
+    public interface IOrderRepository
+    {
+        Order GetSingle(int id);
+        void Add(Order order);
+        bool Save();
+    }
+}
---
+++ PackagingCalculator/Repositories/OrderRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PackagingCalculator.Entities;
+
+namespace PackagingCalculator.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OrderDbContext _orderDbContext;
+
+        public OrderRepository(OrderDbContext orderDbContext)
+        {
+            _orderDbContext = orderDbContext;
+        }
+
+        public Order GetSingle(int id)
+        {
+            return _orderDbContext.Orders.FirstOrDefault(x => x.OrderID == id);
+        }
+
+        public void Add(Order order)
+        {
+            _orderDbContext.Orders.Add(order);
+            
+        }
+
+        public bool Save()
+        {
+            return (_orderDbContext.SaveChanges() >= 0);
+        }
+    }
+}
